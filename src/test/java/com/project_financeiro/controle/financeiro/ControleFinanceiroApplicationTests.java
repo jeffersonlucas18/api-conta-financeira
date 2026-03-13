29aec64642a249ee3602a2161e490becf0d825a9
Jefferson Lucas <jefferson@dimensiva.com.br>
2026-03-13 19:21:59 -0300

    teste

---
+++ src/test/java/com/project_financeiro/controle/financeiro/ControleFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_financeiro.controle.financeiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
